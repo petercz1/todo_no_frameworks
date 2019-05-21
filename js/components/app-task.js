24be8b41f9ce212df74d2b1c0c8d38a5057267c6
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 11:47:59 +0800

    Your reasoning is excellent -- it's only your basic assumptions that are wrong.

--- js/components/app-task.js
+++ js/components/app-task.js
@@ -14,8 +14,8 @@ class appTask extends RootElement {
   renderData() {
     this.innerHTML = `
     <div class="task">
-      <input type="checkbox" id="${this.task.id}" />
-      <label for="${this.task.id}" class="label">${this.task.taskname}</label>
+    <label for="${this.task.id}" class="label">${this.task.taskname}</label>
+    <input type="checkbox" id="${this.task.id}" />
       <button class="delete">&#128465;</button></br>
     </div>
     `;
