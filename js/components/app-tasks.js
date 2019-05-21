198ec232dd30ef33a592d51015312278d93d799d
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 16:15:19 +0800

    Your reasoning is excellent -- it's only your basic assumptions that are wrong.

--- js/components/app-tasks.js
+++ js/components/app-tasks.js
@@ -13,7 +13,6 @@ class appTasks extends RootElement {
 
   // renders all people returned from 'getTasks()'
   renderData(tasks) {
-    this.setAttribute('class', 'tasks');
     this.innerHTML = '';
     tasks.forEach(task => {
       this.append(new AppTask(task));
