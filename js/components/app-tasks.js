b2fdeb9917c721276f19faf09ffcd5fb158c6f2d
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 15:20:59 +0800

    You have no real enemies.

--- js/components/app-tasks.js
+++ js/components/app-tasks.js
@@ -20,7 +20,7 @@ class appTasks extends RootElement {
     // append task elements
     tasks.forEach(task => {
       // this.querySelector('#tasks').append(new AppTask(task));
-      this.append(new AppTask(task));
+      this.appendChild(new AppTask(task));
     });
   }
 }
