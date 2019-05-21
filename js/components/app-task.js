a6b1aad0236cb0ab5e4701226bc9f9d246798b2f
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 15:58:43 +0800

    You have no real enemies.

--- js/components/app-task.js
+++ js/components/app-task.js
@@ -19,7 +19,7 @@ class appTask extends RootElement {
       <p>&#128465;</p>
     `;
     this.querySelector('input').addEventListener('change', this.registerChange);
-    this.querySelector('p').addEventListener('click', this.registerDelete);
+    this.querySelector('span').addEventListener('click', this.registerDelete);
   }
 
   registerChange() {
