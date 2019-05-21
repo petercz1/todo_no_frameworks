f5cc7f80d236b54a89b6073601380552f8ff5788
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 09:52:25 +0800

    You have no real enemies.

--- js/pubsub/datastore.js
+++ js/pubsub/datastore.js
@@ -11,7 +11,7 @@ class DataStore {
   // I added 'listener.data' in case someone wants to extend the idea
   // by passing a variable eg id (not implemented here)
   getRequest(subscriber) {
-    cl
+    console.log(this.tasks);
     return this[subscriber.request](subscriber.parameters);
   }
 
