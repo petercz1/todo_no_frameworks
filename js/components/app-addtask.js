5171c2cdd6d7a2bd065f4420a405e34392870958
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 16:41:16 +0800

    You have Egyptian flu: you're going to be a mummy.

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -4,7 +4,9 @@ import PubSub from '../data/pubsub.js';
 class appAddTask extends RootElement {
   constructor() {
     super();
-    this.pubsub = PubSub;
+	this.pubsub = PubSub;
+	let tasks = ['watch GOT', 'check FB', 'wash dishes', 'refine plans for Death Star', 'invent killer app', 'practice sarcasm', 'make fun of hipsters', 'drink more coffee','google "pimp my noodles"',
+				'complain more often', 'spike watercooler', 'write "idiot filter" for gmail', 'ignore todo list','beat personal best on snake'];
     this.renderData();
   }
 
