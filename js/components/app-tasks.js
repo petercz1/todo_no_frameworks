2564f2ac72c4f35b584f6a569f1edd0755493989
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 14:06:27 +0800

    If more of us valued food and cheer and song above hoarded gold, it would be a merrier world. 		-- J.R.R. Tolkien

--- js/components/app-tasks.js
+++ js/components/app-tasks.js
@@ -18,7 +18,7 @@ class appTasks extends RootElement {
     `;
     // append task elements
     tasks.forEach(task => {
-      this.querySelector('#tasks').append(new AppTask(task));
+      this.querySelector('app-tasks').append(new AppTask(task));
     });
   }
 }
