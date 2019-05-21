d794fcfe671f57db16cd407d20fa65ce731997c6
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 16:50:19 +0800

    You are as I am with You.

--- js/components/app-console.js
+++ js/components/app-console.js
@@ -9,7 +9,6 @@ class appConsole extends RootElement {
 		this.renderData(this.pubsub.getData('getPeople', null));
 		this.pubsub.subscribe('ChangeTask', 'getPeople', null, this.renderData);
 		this.pubsub.subscribe('DeleteTask', 'getPeople', null, this.renderData);
-		this.pubsub.subscribe('DeletePerson', 'getPeople', null, this.renderData);
   }
 
 	// renders all data from 'getPeople()'
