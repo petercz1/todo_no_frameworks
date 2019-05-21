450d0bce1079d83e6170db8c6a50955cdb58ada3
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 15:28:55 +0800

    You are capable of planning your future.

--- js/components/app-tasks.js
+++ js/components/app-tasks.js
@@ -9,7 +9,6 @@ class appTasks extends RootElement {
     this.renderData(this.pubsub.getData('getTasks',null));
     this.pubsub.subscribe('NewTask', 'getTasks', null, this.renderData);
     this.pubsub.subscribe('DeleteTask', 'getTasks', null, this.renderData);
-    this.apptask = new AppTask;
   }
 
   // renders all people returned from 'getTasks()'
