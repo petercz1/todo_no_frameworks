d607ff098d9fb69bf4275f87411eeeb99588d7c8
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 10:06:39 +0800

    Executive ability is prominent in your make-up.

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -29,7 +29,7 @@ class appAddTask extends RootElement {
 
   // random task generator for testing, saves having to type a value into the input box
   generateTask() {
-    let tasks = ['watch GOT', 'check FB', 'wash dishes', 'refine plans for Death Star', 'invent killer app', 'practice sarcasm', 'make fun of hipsters', 'drink more coffee'];
+    let tasks = ['watch GOT', 'check FB', 'wash dishes', 'refine plans for Death Star', 'invent killer app', 'practice sarcasm', 'make fun of hipsters', 'drink more coffee',''];
     this.querySelector('#task').value = tasks[Math.floor(Math.random() * tasks.length)];
   }
 }
