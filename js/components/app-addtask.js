048922149e9784e3407a0df402eaea92230dac64
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 16:41:30 +0800

    Someone is speaking well of you.

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -5,7 +5,7 @@ class appAddTask extends RootElement {
   constructor() {
     super();
 	this.pubsub = PubSub;
-	// 
+	// used for generating 
 	this.tasks = ['watch GOT', 'check FB', 'wash dishes', 'refine plans for Death Star', 'invent killer app', 'practice sarcasm', 'make fun of hipsters', 'drink more coffee','google "pimp my noodles"',
 				'complain more often', 'spike watercooler', 'write "idiot filter" for gmail', 'ignore todo list','beat personal best on snake'];
     this.renderData();
