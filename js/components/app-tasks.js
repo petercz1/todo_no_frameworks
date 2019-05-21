62d9f776c4868a7db1a3c52fdcb61ef6d79ce669
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 15:29:41 +0800

    You will become rich and famous unless you don't.

--- js/components/app-tasks.js
+++ js/components/app-tasks.js
@@ -21,7 +21,7 @@ class appTasks extends RootElement {
     // append task elements
     tasks.forEach(task => {
       // this.querySelector('#tasks').append(new AppTask(task));
-      this.listOfTasks +=
+      this.listOfTasks += new App
       this.append(new AppTask(task));
     });
   }
