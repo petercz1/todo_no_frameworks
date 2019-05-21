28a6a6094d338edc982671006c43d5ef154d8430
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 10:30:28 +0800

    Q:	Why don't lawyers go to the beach? A:	The cats keep trying to bury them.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
 // import webcomponents
 import './components/app-rootelement.js';
+import './components/app-addtasks.js';
 import './components/app-addtask.js';
-import './components/app-addtask.js';
-
-
-// import './components/app-people.js';
-// import './components/app-messages.js';
-// import './components/app-console.js';
-// import './components/app-chosenpeople.js';
-// import './components/app-chosenperson.js';
