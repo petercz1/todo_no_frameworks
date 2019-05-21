476f8365c2bc4df918cf1efe1a9b15fa88ea7a20
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 15:01:36 +0800

    "... all the modern inconveniences ..." 		-- Mark Twain

--- js/components/app-tasks.js
+++ js/components/app-tasks.js
@@ -19,7 +19,7 @@ class appTasks extends RootElement {
     // `;
     // append task elements
     tasks.forEach(task => {
-      this.querySelector('#tasks').append(new AppTask(task));
+      // this.querySelector('#tasks').append(new AppTask(task));
       this.querySelector('#tasks').append(new AppTask(task));
     });
   }
