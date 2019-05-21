ef41f15b584593fadc7b9b06694c2c0676be06c5
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 16:44:21 +0800

    Don't go around saying the world owes you a living.  The world owes you nothing.  It was here first. 		-- Mark Twain

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -35,6 +35,7 @@ class appAddTask extends RootElement {
 	let item = this.tasks[Math.floor(Math.random() * this.tasks.length)];
 	this.querySelector('#taskname').value = item;
 	this.tasks = this.tasks.filter(task =>{ return task != item})
+	console.log(this.tasks);
   }
 }
 
