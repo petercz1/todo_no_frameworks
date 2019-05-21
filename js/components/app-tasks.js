928af06518ba6907eaafc2ec3804acb491a7dbb1
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 15:27:36 +0800

    You plan things that you do not even attempt because of your extreme caution.

--- js/components/app-tasks.js
+++ js/components/app-tasks.js
@@ -21,7 +21,7 @@ class appTasks extends RootElement {
     // append task elements
     tasks.forEach(task => {
       // this.querySelector('#tasks').append(new AppTask(task));
-      
+      sinfgle
       this.append(new AppTask(task));
     });
   }
