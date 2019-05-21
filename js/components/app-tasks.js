d918cf3482538a06b12568dcb885758546b450de
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 15:33:27 +0800

    Q:	What do you call the scratches that you get when a female 	sheep bites you? A:	Ewe nicks.

--- js/components/app-tasks.js
+++ js/components/app-tasks.js
@@ -9,7 +9,7 @@ class appTasks extends RootElement {
     this.renderData(this.pubsub.getData('getTasks',null));
     this.pubsub.subscribe('NewTask', 'getTasks', null, this.renderData);
     this.pubsub.subscribe('DeleteTask', 'getTasks', null, this.renderData);
-    this.listOfTasks = new HTML;
+    this.listOfTasks = new HTMLE;
   }
 
   // renders all people returned from 'getTasks()'
