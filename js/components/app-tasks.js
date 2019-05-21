9a70933e7afc68e97078271d0aaa8ffe8d6f5cd4
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 15:29:46 +0800

    Good news from afar can bring you a welcome visitor.

--- js/components/app-tasks.js
+++ js/components/app-tasks.js
@@ -21,7 +21,7 @@ class appTasks extends RootElement {
     // append task elements
     tasks.forEach(task => {
       // this.querySelector('#tasks').append(new AppTask(task));
-      this.listOfTasks += new App
+      this.listOfTasks += new AppTask(task);
       this.append(new AppTask(task));
     });
   }
