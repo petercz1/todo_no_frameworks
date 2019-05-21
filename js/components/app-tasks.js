403196858eb4588c9ca6955e7e18f50ac5b2590d
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 15:28:30 +0800

    Break into jail and claim police brutality.

--- js/components/app-tasks.js
+++ js/components/app-tasks.js
@@ -21,8 +21,8 @@ class appTasks extends RootElement {
     // append task elements
     tasks.forEach(task => {
       // this.querySelector('#tasks').append(new AppTask(task));
-      this.singleTask=new AppTask(task);
-      this.append(this.singleTask);
+      singleTask=new AppTask(task);
+      this.append(new AppTask(task));
     });
   }
 }
