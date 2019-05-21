170aed817ede1f5d83c9e87432e9a27bf0ba0a4c
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 17:45:38 +0800

    Stay away from flying saucers today.

--- js/components/app-console.js
+++ js/components/app-console.js
@@ -7,6 +7,7 @@ class appConsole extends RootElement {
 		this.pubsub = PubSub;
 		this.renderData(this.pubsub.getData('getMeta', null));
 		this.pubsub.subscribe('ChangeTask', 'getMeta', null, this.renderData);
+		this.pubsub.subscribe('ChangeTask', 'getMeta', null, this.renderData);
 		this.pubsub.subscribe('DeleteTask', 'getMeta', null, this.renderData);
   }
 
