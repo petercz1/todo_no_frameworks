b9ddc42cf7ed39c409a7f0a72ce0baae2df3268a
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 15:29:11 +0800

    Future looks spotty.  You will spill soup in late evening.

--- js/components/app-tasks.js
+++ js/components/app-tasks.js
@@ -9,7 +9,7 @@ class appTasks extends RootElement {
     this.renderData(this.pubsub.getData('getTasks',null));
     this.pubsub.subscribe('NewTask', 'getTasks', null, this.renderData);
     this.pubsub.subscribe('DeleteTask', 'getTasks', null, this.renderData);
-    this.listoftasks
+    this.listOftasks = '';
   }
 
   // renders all people returned from 'getTasks()'
