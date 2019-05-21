72111790fabb8ef92e35823e3d8f823271731829
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 17:18:40 +0800

    You possess a mind not merely twisted, but actually sprained.

--- js/main.js
+++ js/main.js
@@ -3,3 +3,4 @@ import './components/app-rootelement.js';
 import './components/app-tasks.js';
 import './components/app-task.js';
 import './components/app-addtask.js';
+import './components/app-addtask.js';
