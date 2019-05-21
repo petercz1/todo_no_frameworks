a7ba72748225d83e4fa2ce607a8e7ba17624e33f
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 09:36:05 +0800

    You possess a mind not merely twisted, but actually sprained.

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -10,7 +10,6 @@ class appAddTask extends RootElement {
 
   renderData() {
     this.innerHTML = `
-      <small>&lt;app-addtask&gt;</small>
       <h2>Enter a task</h2>
       <input type="text" id="task" value='bruno'></input>
       <button id="newPerson">add person</button>
