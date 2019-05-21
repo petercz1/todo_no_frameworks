3081fafa375dc588cd30c8e4e542513cdb07779c
petercz1 <email.peter.carroll@gmail.com>
2019-05-21 16:41:04 +0800

    Every cloud engenders not a storm. 		-- William Shakespeare, "Henry VI"

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -29,8 +29,7 @@ class appAddTask extends RootElement {
 
   // random task generator for testing, saves having to type a value into the input box
   generateTask() {
-	let tasks = ['watch GOT', 'check FB', 'wash dishes', 'refine plans for Death Star', 'invent killer app', 'practice sarcasm', 'make fun of hipsters', 'drink more coffee','google "pimp my noodles"',
-				'complain more often', 'spike watercooler', 'write "idiot filter" for gmail', 'ignore todo list','beat personal best on snake'];
+	
 	let item = tasks[Math.floor(Math.random() * tasks.length)];
 	this.querySelector('#taskname').value = item;
 	
