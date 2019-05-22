aae5a142472a9c2bd49a6505ef6a92d81cfb6f4f
petercz1 <email.peter.carroll@gmail.com>
2019-05-22 15:43:33 +0800

    Q:	Why did the astrophysicist order three hamburgers? A:	Because he was hungry.

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -34,7 +34,7 @@ class appAddTask extends RootElement {
   // random task generator for testing, saves having to make up a task and type it into the input box
   generateTask() {
     let item = this.tasks[Math.floor(Math.random() * this.tasks.length)];
-    setTimeout(
+    setTimeout(()=>{
       this.querySelector('#taskname').value = item, 500);
     this.tasks = this.tasks.filter(task => {
       return task != item
