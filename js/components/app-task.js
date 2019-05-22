fdadb28d4cee41180684b8f1df4117b879d53e9a
petercz1 <email.peter.carroll@gmail.com>
2019-05-22 15:11:40 +0800

    You will experience a strong urge to do good; but it will pass.

--- js/components/app-task.js
+++ js/components/app-task.js
@@ -28,7 +28,6 @@ class appTask extends RootElement {
       setTimeout(() => {
         this.querySelector('div').classList.replace('closed','open');
       }, 100);
-      this.querySelector('div').classList.remove('open');
     }
   }
 
