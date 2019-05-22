02fbb52b6065f08d39fa60d8f3ec0f1e249ed175
petercz1 <email.peter.carroll@gmail.com>
2019-05-22 15:35:25 +0800

    You are magnetic in your bearing.

--- js/components/app-task.js
+++ js/components/app-task.js
@@ -39,7 +39,7 @@ class appTask extends RootElement {
   }
 
   registerDelete() {
-    this.querySelector('div').classList.add('closed');
+    this.querySelector('div').classList.replace('closed');
     this.task.deleted = true;
     this.pubsub.publish('DeleteTask', this.task);
   }
