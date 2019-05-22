27348e599ca762cc4283ed37e00768f357208a1d
petercz1 <email.peter.carroll@gmail.com>
2019-05-22 15:10:59 +0800

    As to the Adjective: when in doubt, strike it out. 		-- Mark Twain, "Pudd'nhead Wilson's Calendar"

--- js/components/app-task.js
+++ js/components/app-task.js
@@ -28,7 +28,7 @@ class appTask extends RootElement {
       setTimeout(() => {
         this.querySelector('div').classList.replace('closed','open');
       }, 100);
-      this.querySelector('div').classList.remove('''open');
+      this.querySelector('div').classList.remove('open');
     }
   }
 
