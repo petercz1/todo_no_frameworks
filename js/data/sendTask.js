3b8218aa6d19f2178439964a6715534256ae10b6
petercz1 <email.peter.carroll@gmail.com>
2019-05-22 15:59:47 +0800

    "You have been in Afghanistan, I perceive." 		-- Sir Arthur Conan Doyle, "A Study in Scarlet"

--- js/data/sendTask.js
+++ js/data/sendTask.js
@@ -6,7 +6,7 @@ export default new class SendData {
   constructor() {
     this.pubsub = PubSub;
     this.sendData = this.sendData.bind(this);
-    this.pubsub.subscribe('SendLocalData', 'getLocalData', null, this.sendData);
+    this.pubsub.subscribe('NewTask', 'getLocalData', null, this.sendData);
   }
 
   async sendData(task) {
