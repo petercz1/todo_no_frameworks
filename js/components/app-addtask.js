1a0b1d6bf0c57d67312fad8bd4992bdb6195b7e0
petercz1 <email.peter.carroll@gmail.com>
2019-05-22 15:42:57 +0800

    Many changes of mind and mood; do not hesitate too long.

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -4,19 +4,19 @@ import PubSub from '../data/pubsub.js';
 class appAddTask extends RootElement {
   constructor() {
     super();
-	this.pubsub = PubSub;
-	// used for generating random tasks for testing
-	this.tasks = ['watch GOT', 'check FB', 'wash dishes', 'refine plans for Death Star', 'invent killer app', 'practice sarcasm', 'make fun of hipsters', 'drink more coffee','google "pimp my noodles"','complain more often', 'spike watercooler', 'write "idiot filter" for gmail', 'ignore todo list','beat personal best on snake'];
+    this.pubsub = PubSub;
+    // used for generating random tasks for testing
+    this.tasks = ['watch GOT', 'check FB', 'wash dishes', 'refine plans for Death Star', 'invent killer app', 'practice sarcasm', 'make fun of hipsters', 'drink more coffee', 'google "pimp my noodles"', 'complain more often', 'spike watercooler', 'write "idiot filter" for gmail', 'ignore todo list', 'beat personal best on snake'];
     this.renderData();
   }
 
   renderData() {
-	this.innerHTML = `
+    this.innerHTML = `
 	<input type="text" id="taskname" placeholder="your new task"></input>
 	<button id="newTask">add task</button>
 	`;
-	// generate random new task
-	this.generateTask();	
+    // generate random new task
+    this.generateTask();
     document.querySelector('#newTask').addEventListener('click', this.addtask);
   }
 
@@ -26,17 +26,19 @@ class appAddTask extends RootElement {
       checked: false,
       css: 'closed'
     };
-	// generate new random task
-	this.generateTask();
-	this.pubsub.publish('NewTask', task);
+    // generate new random task
+    this.generateTask();
+    this.pubsub.publish('NewTask', task);
   }
 
   // random task generator for testing, saves having to make up a task and type it into the input box
   generateTask() {
-  let item = this.tasks[Math.floor(Math.random() * this.tasks.length)];
-  setTimeout(
-	this.querySelector('#taskname').value = item;
-	this.tasks = this.tasks.filter(task =>{ return task != item})
+    let item = this.tasks[Math.floor(Math.random() * this.tasks.length)];
+    setTimeout(
+      this.querySelector('#taskname').value = item, 500);
+    this.tasks = this.tasks.filter(task => {
+      return task != item
+    })
   }
 }
 
