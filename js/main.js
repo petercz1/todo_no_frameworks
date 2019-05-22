2e4368cd0bfa36a7969e046954e117cce6bf1944
petercz1 <email.peter.carroll@gmail.com>
2019-05-22 16:02:57 +0800

    Q:	What do you call a principal female opera singer whose high C 	is lower than those of other principal female opera singers? A:	A deep C diva.

--- js/main.js
+++ js/main.js
@@ -4,4 +4,4 @@ import './components/app-tasks.js';
 import './components/app-task.js';
 import './components/app-addtask.js';
 import './components/app-console.js';
-import './components/app-console.js';
+import './data/';
