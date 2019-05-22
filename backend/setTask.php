bb407f8faa4c5bfbf2b42ef97a71bf123c57a73f
petercz1 <email.peter.carroll@gmail.com>
2019-05-22 16:09:04 +0800

    While you recently had your problems on the run, they've regrouped and are making another attack.

--- backend/setTask.php
+++ backend/setTask.php
@@ -6,7 +6,7 @@ class SetTask{
 
 	public function __construct(){
 		echo "{'server':'working'}";
-		error_log()
+		error_log($_POST, 'debug.log')
 		$data = $_POST;
 		//echo json_encode($data);
 	}
