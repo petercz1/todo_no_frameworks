3c753a86be8609cb7d5d722bffac383ce564ca5f
petercz1 <email.peter.carroll@gmail.com>
2019-05-22 16:08:12 +0800

    Q:	What does it say on the bottom of Coke cans in North Dakota? A:	Open other end.

---
+++ backend/setTask.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+namespace chipbug\todo;
+
+class SetTask{
+
+	public function __construct(){
+		echo "{'server':'working'}";
+		$data = $_POST;
+		//echo json_encode($data);
+	}
+}
