639de345eb3c32138f55f155dfb5caad877aad8a
petercz1 <email.peter.carroll@gmail.com>
2019-05-23 10:08:02 +0800

    It's a very *__UN*lucky week in which to be took dead. 		-- Churchy La Femme

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -35,6 +35,7 @@ class appAddTask extends RootElement {
     let tasks = ['watch GOT', 'check FB', 'wash dishes', 'refine plans for Death Star', 'invent killer app', 'practice sarcasm', 'make fun of hipsters', 'drink more coffee', 'google "pimp my noodles"', 'complain more often', 'spike watercooler', 'write "idiot filter" for gmail', 'ignore todo list', 'beat personal best on snake'];
     let item = tasks[Math.floor(Math.random() * tasks.length)];
     // pseudo-typewriter effect, because I was bored
+    
     (type()=>{
       if()
     })();
