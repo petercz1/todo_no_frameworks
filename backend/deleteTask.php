daf6b52e2163c7a7a07962633ebe4db67568c766
petercz1 <email.peter.carroll@gmail.com>
2019-05-23 14:12:13 +0800

    Among the lucky, you are the chosen one.

--- backend/deleteTask.php
+++ backend/deleteTask.php
@@ -2,7 +2,27 @@
 declare(strict_types=1);
 namespace chipbug\todo;
 
+ini_set("log_errors", "1");
+ini_set("error_log", getcwd() . "/debug.log");
+
+(new DeleteTask)->init();
+
 class DeleteTask
 {
-    // code
+    public function init()
+    {
+        $body = file_get_contents('php://input');
+        error_log(print_r($body, true));
+        $this->updateTask($body);
+    }
+
+    public function updateTask($data)
+    {
+        // sort it!
+        $tasks = json_decode(\file_get_contents('tasks.json'));
+        array_unshift($tasks, $data);
+        \error_log(print_r($tasks, true));
+        file_put_contents('tasks.json', json_encode($tasks));
+        echo json_encode($tasks);
+    }
 }
