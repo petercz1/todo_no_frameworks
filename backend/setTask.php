2c6d905be16a5f0b87d65c9bdc6e8eaee9c33101
petercz1 <email.peter.carroll@gmail.com>
2019-05-23 12:25:36 +0800

    Q:	What do little WASPs want to be when they grow up? A:	The very best person they can possibly be.

--- backend/setTask.php
+++ backend/setTask.php
@@ -23,6 +23,5 @@ class SetTask
         $tasks = json_decode(\file_get_contents('tasks.json'));
         array_unshift($tasks, $data);
         file_put_contents('tasks.json', json_encode($tasks));
-
     }
 }
