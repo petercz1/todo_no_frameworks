a121527d2e15b3e2b38b94fbb3e5be931f1694b3
petercz1 <email.peter.carroll@gmail.com>
2019-05-23 10:17:28 +0800

    Tell the truth or trump--but get the trick. 		-- Mark Twain, "Pudd'nhead Wilson's Calendar"

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -39,6 +39,7 @@ class appAddTask extends RootElement {
     let counter = 0;
     type();
     function type(){
+      super(this)
       if(counter < item.length){
         this.querySelector('#taskname').value += item.charAt(counter);
         counter ++;
