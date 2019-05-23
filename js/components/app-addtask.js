235e20cc1c723ff37f197cf6cb291dcca4c25bde
petercz1 <email.peter.carroll@gmail.com>
2019-05-23 10:06:21 +0800

    You will never know hunger.

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -35,7 +35,7 @@ class appAddTask extends RootElement {
     let tasks = ['watch GOT', 'check FB', 'wash dishes', 'refine plans for Death Star', 'invent killer app', 'practice sarcasm', 'make fun of hipsters', 'drink more coffee', 'google "pimp my noodles"', 'complain more often', 'spike watercooler', 'write "idiot filter" for gmail', 'ignore todo list', 'beat personal best on snake'];
     let item = tasks[Math.floor(Math.random() * tasks.length)];
     // pseudo-typewriter effect, because I was bored
-    ()
+    ()();
     for (let counter = 0; counter < item.length; counter++) {
       setTimeout(() => {
         console.log(item.charAt(counter));
