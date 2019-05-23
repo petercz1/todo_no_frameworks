805128e2f420aafd1b5ab72c0f664bd40a9d89ca
petercz1 <email.peter.carroll@gmail.com>
2019-05-23 12:19:43 +0800

    You are going to have a new love affair.

--- backend/setTask.php
+++ backend/setTask.php
@@ -14,6 +14,7 @@ class SetTask
         $body = file_get_contents('php://input');
         error_log(print_r($body, true));
         echo json_encode($body);
+        
     }
 
     public function setTask(data)
