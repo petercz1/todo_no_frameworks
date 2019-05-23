8f65dae1602233e321b91906b87459e2d4c0e559
petercz1 <email.peter.carroll@gmail.com>
2019-05-23 16:24:26 +0800

    The human race is a race of cowards; and I am not only marching in that procession but carrying a banner. 		-- Mark Twain

--- js/components/app-task.js
+++ js/components/app-task.js
@@ -38,7 +38,7 @@ class appTask extends RootElement {
   registerChange() {
     // publish change if task is checked
     this.task.checked = !this.task.checked;
-    this.taskstatus = 'update';
+    this.task.status = 'update';
     this.renderData();
     this.pubsub.publish('ChangeTask', this.task);
   }
