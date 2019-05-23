52d3740b287936a7014fa22a424800c2a053f964
petercz1 <email.peter.carroll@gmail.com>
2019-05-23 10:26:39 +0800

    You have a deep interest in all that is artistic.

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -33,6 +33,7 @@ class appAddTask extends RootElement {
   generateNewTask() {
     this.querySelector('#taskname').value = '';
     let tasks = ['watch GOT', 'check FB', 'wash dishes', 'refine plans for Death Star', 'invent killer app', 'practice sarcasm', 'make fun of hipsters', 'drink more coffee', 'google "pimp my noodles"', 'complain more often', 'spike watercooler', 'write "idiot filter" for gmail', 'ignore todo list', 'beat personal best on snake'];
+    // select a task
     let item = tasks[Math.floor(Math.random() * tasks.length)];
     this.querySelector('#taskname').value += item.charAt(counter);
 
