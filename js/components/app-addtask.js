f3126300dd630694b5dca91b0f86237ffb2a3302
petercz1 <email.peter.carroll@gmail.com>
2019-05-23 10:31:53 +0800

    You are scrupulously honest, frank, and straightforward.  Therefore you have few friends.

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -35,7 +35,8 @@ class appAddTask extends RootElement {
     let tasks = ['watch GOT', 'check FB', 'wash dishes', 'refine plans for Death Star', 'invent killer app', 'practice sarcasm', 'make fun of hipsters', 'drink more coffee', 'google "pimp my noodles"', 'complain more often', 'spike watercooler', 'write "idiot filter" for gmail', 'ignore todo list', 'beat personal best on snake'];
     // randomly select a task
     let item = tasks[Math.floor(Math.random() * tasks.length)];
-    let counter =o;
+    let counter = 0;
+
     this.querySelector('#taskname').value = item;
 
     // remove item from random tasklist
