6798836ce0140dc0d55e4f3dff58720f978d9935
petercz1 <email.peter.carroll@gmail.com>
2019-05-23 12:47:25 +0800

    Q:	What's the difference betweeen USL and the Graf Zeppelin? A:	The Graf Zeppelin represented cutting edge technology for its time.

--- backend/getTasks.php
+++ backend/getTasks.php
@@ -2,7 +2,28 @@
 declare(strict_types=1);
 namespace chipbug\todo;
 
-class GetTasks
+ini_set("log_errors", "1");
+ini_set("error_log", getcwd() . "/debug.log");
+
+(new SetTask)->init();
+
+class SetTask
 {
-// code
+    public function init()
+    {
+        $body = file_get_contents('php://input');
+        error_log(print_r($body, true));
+        //echo json_encode($body);
+        $this->setTask($body);
+    }
+
+    public function setTask($data)
+    {
+        // sort it!
+        $tasks = json_decode(\file_get_contents('tasks.json'));
+        array_unshift($tasks, $data);
+        \error_log(print_r($tasks, true));
+        file_put_contents('tasks.json', json_encode($tasks));
+        echo json_encode($tasks);
+    }
 }
