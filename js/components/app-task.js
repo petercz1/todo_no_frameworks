ff31ca74126c63a266aa06166c1d1b644045b43a
petercz1 <email.peter.carroll@gmail.com>
2019-05-23 14:46:24 +0800

    Your sister swims out to meet troop ships.

--- js/components/app-task.js
+++ js/components/app-task.js
@@ -35,7 +35,7 @@ class appTask extends RootElement {
   registerChange() {
     // publish change if task is checked
     this.task.checked = !this.task.checked;
-    if(this.c)
+    if(this.task.checked)
     this.querySelector('label').classList.toggle('checked');
     this.pubsub.publish('ChangeTask', this.task);
   }
