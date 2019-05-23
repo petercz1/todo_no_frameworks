62cac7b96a953571ec784b1805b6b0bbbf50deb2
petercz1 <email.peter.carroll@gmail.com>
2019-05-23 14:44:26 +0800

    You will be Told about it Tomorrow.  Go Home and Prepare Thyself.

---
+++ js/components/app-task.js
@@ -0,0 +1,49 @@
+import RootElement from './app-rootelement.js';
+import PubSub from '../data/pubsub.js';
+
+class appTask extends RootElement {
+  constructor(task) {
+    super();
+    this.pubsub = PubSub;
+    this.task = task;
+    this.renderData();
+  }
+
+  renderData() {
+    this.innerHTML = `
+    <div class="${this.task.css}">
+      <label for="${this.task.id}" class="label">${this.task.taskname}</label>
+      <input type="checkbox" id="${this.task.id}" />
+      <span>&#128465;</span>
+    </div>
+    `;
+    this.querySelector('input').checked = this.task.checked;
+    this.querySelector('input').addEventListener('change', this.registerChange);
+    this.querySelector('span').addEventListener('click', this.registerDelete);
+  }
+
+  // setTimeout needed because connectedCallback doesn't wait for child elements to render
+  connectedCallback() {
+    if (this.task.css) {
+      setTimeout(() => {
+        this.querySelector('div').classList.replace('closed', 'open');
+      }, 100);
+      this.pubsub.publish('DisplayedTask', this.task);
+    }
+  }
+
+  registerChange() {
+    // publish change if task is checked
+    this.task.checked = !this.task.checked;
+    this.querySelector('input')
+    this.pubsub.publish('ChangeTask', this.task);
+  }
+
+  registerDelete() {
+    this.pubsub.publish('DeleteTask', this.task);
+  }
+}
+
+export default appTask;
+
+customElements.define('app-task', appTask);
