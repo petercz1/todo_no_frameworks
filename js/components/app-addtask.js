3abbb274037a259434244f96311d05efcddfb829
petercz1 <email.peter.carroll@gmail.com>
2019-05-23 10:27:56 +0800

    You will be run over by a bus.

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -31,7 +31,6 @@ class appAddTask extends RootElement {
 
   // random task generator for testing, saves having to make up a task and type it into the input box
   generateNewTask() {
-    this.querySelector('#taskname').value = '';
     let tasks = ['watch GOT', 'check FB', 'wash dishes', 'refine plans for Death Star', 'invent killer app', 'practice sarcasm', 'make fun of hipsters', 'drink more coffee', 'google "pimp my noodles"', 'complain more often', 'spike watercooler', 'write "idiot filter" for gmail', 'ignore todo list', 'beat personal best on snake'];
     // randomly select a task
     let item = tasks[Math.floor(Math.random() * tasks.length)];
