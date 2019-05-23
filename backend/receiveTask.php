74efca7478e71ee96e5bdcb7a3cdab58b43d7f67
petercz1 <email.peter.carroll@gmail.com>
2019-05-23 20:17:00 +0800

    Save energy: be apathetic.

--- backend/receiveTask.php
+++ backend/receiveTask.php
@@ -5,9 +5,9 @@ namespace chipbug\todo;
 ini_set("log_errors", "1");
 ini_set("error_log", getcwd() . "/debug.log");
 
-(new AddTask)->init();
+(new RecieveTask)->init();
 
-class AddTask
+class RecieveTask
 {
     public function init()
     {
