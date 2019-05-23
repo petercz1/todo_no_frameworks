1f62e85ed1f49c41d6139d52c646262fcf0d3b38
petercz1 <email.peter.carroll@gmail.com>
2019-05-23 20:18:05 +0800

    Your goose is cooked. (Your current chick is burned up too!)

---
+++ backend/receiveTask.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+namespace chipbug\todo;
+
+ini_set("log_errors", "1");
+ini_set("error_log", getcwd() . "/debug.log");
+
+(new ReceiveTask)->init();
+
+class ReceiveTask
+{
+    public function init()
+    {
+        $body = json_decode(file_get_contents('php://input'), true);
+        \error_log(print_r($body, true));
+        switch ($body['status']) {
+            case 'new':
+            $this->addTask($body);
+            break;
+            case 'update':
+            $this->updateTask($body);
+            break;
+            case 'delete':;
+            $thi->delete($body);
+            break;
+        }
+    }
+
+    public function addTask($data)
+    {
+        $tasks = json_decode(\file_get_contents('tasks.json'));
+        array_unshift($tasks, $data);
+        file_put_contents('tasks.json', json_encode($tasks));
+        echo '{"server":"added task"}';
+    }
+
+    public function updateTask($data)
+    {
+        # code...
+    }
+
+    public function deleteTask($data)
+    {
+        
+    }
+}
