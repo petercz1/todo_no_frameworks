ce155474beb543c57d2e77ef218e2a86cd11baf6
petercz1 <email.peter.carroll@gmail.com>
2019-05-23 10:14:16 +0800

    It's all in the mind, ya know.

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -37,7 +37,7 @@ class appAddTask extends RootElement {
 
     // pseudo-typewriter effect, because I was bored
     let counter = 0;
-    //type();
+    type();
     function type(){
       if(counter < item.length){
         this.querySelector('#taskname').value += item.charAt(counter);
