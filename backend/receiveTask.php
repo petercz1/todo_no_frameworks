6bfd0505da31463e9489dc347d80d9b5ec34c467
petercz1 <email.peter.carroll@gmail.com>
2019-05-23 17:04:17 +0800

    Be free and open and breezy!  Enjoy!  Things won't get any better so get used to it.

---
+++ backend/receiveTask.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+namespace chipbug\todo;
+
+ini_set("log_errors", "1");
+ini_set("error_log", getcwd() . "/debug.log");
+
+(new AddTask)->init();
+
+class AddTask
+{
+    public function init()
+    {
+        $body = json_decode(file_get_contents('php://input'), true);
+        \error_log(print_r($body, true));
+        switch($body['status']){
+            case 'new':
+            addTask
+        }
+        $this->addTask($body);
+    }
+
+    public function addTask($data)
+    {
+        $tasks = json_decode(\file_get_contents('tasks.json'));
+        array_unshift($tasks, $data);
+        file_put_contents('tasks.json', json_encode($tasks));
+        echo '{"server":"added task"}';
+    }
+}
