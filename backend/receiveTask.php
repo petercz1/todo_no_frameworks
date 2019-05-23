e3081bdd26d9428f3b200e52d49d26f37ee26154
petercz1 <email.peter.carroll@gmail.com>
2019-05-23 17:01:45 +0800

    A light wife doth make a heavy husband. 		-- Wm. Shakespeare, "The Merchant of Venice"

--- backend/receiveTask.php
+++ backend/receiveTask.php
@@ -13,6 +13,7 @@ class AddTask
     {
         $body = json_decode(file_get_contents('php://input'), true);
         \error_log(print_r($body, true));
+        
         $this->addTask($body);
     }
 
