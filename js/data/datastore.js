06daf1a1e145aba5bb4b333b2f01a4274c7902f5
petercz1 <email.peter.carroll@gmail.com>
2019-05-24 12:50:23 +0800

    You will be Told about it Tomorrow.  Go Home and Prepare Thyself.

--- js/data/datastore.js
+++ js/data/datastore.js
@@ -66,7 +66,7 @@ class DataStore {
     console.log(data);
     let taskIndex = this.tasks.findIndex(task => task.id == data.id);
     //task.status = 'delete';
-    this.tasks[]
+    this.tasks[taskIndex]
   }
   getDeleteTask() {
     // filter returns a copy of the array, which then replaces the original
