3d92b5162e29e494778693bfb34dae1bda483bf9
petercz1 <email.peter.carroll@gmail.com>
2019-05-24 10:42:53 +0800

    A is for Apple. 		-- Hester Pryne

---
+++ backend/receiveTask.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+namespace chipbug\todo;
+
+ini_set("log_errors", "1");
+ini_set("error_log", getcwd() . "/debug.log");
+
+(new ReceiveTask)->init();
+
+class ReceiveTask
+{
+    public function init()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+        \error_log(print_r($data, true));
+        if(is_array($data)){
+            foreach ($data as $task){
+                $this->router($task);
+            }
+        }else{
+            $this->router($task);
+        }
+    }
+
+    public function router($task)
+    {
+        switch ($task['status']) {
+            case 'new':
+            $this->addTask($task);
+            break;
+            case 'update':
+            $this->updateTask($task);
+            break;
+            case 'delete':;
+            $this->deleteTask($task);
+            break;
+        }
+    }
+
+    public function addTask($data)
+    {
+        $tasks = json_decode(\file_get_contents('tasks.json'));
+        array_unshift($tasks, $data);
+        file_put_contents('tasks.json', json_encode($tasks));
+        echo '{"server":"added task"}';
+    }
+
+    public function updateTask($data)
+    {
+        echo '{"server":"updated task"}';
+    }
+
+    public function deleteTask($data)
+    {
+        echo '{"server":"deleted task"}';
+    }
+}
