b0ee1421fc3b0454cc9934fe80067b8d9e825dda
petercz1 <email.peter.carroll@gmail.com>
2019-05-24 10:31:11 +0800

    Your present plans will be successful.

--- backend/receiveTask.php
+++ backend/receiveTask.php
@@ -13,7 +13,7 @@ class ReceiveTask
     {
         $body = json_decode(file_get_contents('php://input'), true);
         \error_log(print_r($body, true));
-foreach()
+foreach($body as )
 
         switch ($body['status']) {
             case 'new':
