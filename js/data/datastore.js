a8c8b18db082a8e823343adbf5116fa54d8f8295
petercz1 <email.peter.carroll@gmail.com>
2019-05-24 12:55:14 +0800

    You have been selected for a secret mission.

--- js/data/datastore.js
+++ js/data/datastore.js
@@ -67,8 +67,8 @@ class DataStore {
     // let taskIndex = this.tasks.findIndex(task => task.id == data.id);
     // this.tasks[taskIndex].status == 'delete';
     // console.log(this.tasks);
+    console.log(this.tasks);
     this.updateMeta();
-    console.log(this.);
   }
   getDeleteTask() {
     // filter returns a copy of the array, which then replaces the original
