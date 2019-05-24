8118bf776c0d8628294a27187d35aa9f2731eded
petercz1 <email.peter.carroll@gmail.com>
2019-05-24 12:37:36 +0800

    You will experience a strong urge to do good; but it will pass.

--- js/data/datastore.js
+++ js/data/datastore.js
@@ -64,13 +64,6 @@ class DataStore {
 
   setDeleteTask(data) {
     console.log(data);
-    console.log(this.tasks.filter(task => task.id == data.id));
-    this.tasks = this.tasks.map(task => {
-      if (task.id == data.id) {
-        task.status = 'delete';
-      }
-      return task;
-    });
   }
   getDeleteTask() {
     // filter returns a copy of the array, which then replaces the original
