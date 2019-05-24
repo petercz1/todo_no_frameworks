602b638585190c074decaaa26277f047451d2027
petercz1 <email.peter.carroll@gmail.com>
2019-05-24 10:22:53 +0800

    Every cloud engenders not a storm. 		-- William Shakespeare, "Henry VI"

--- js/data/datastore.js
+++ js/data/datastore.js
@@ -59,7 +59,7 @@ class DataStore {
   }
   getChangeTask() {
     // stubbed out - handled asynchronously in sendData()
-    return this.tasks.fil
+    return this.tasks.filter(task=>)
   }
 
   setDeleteTask(data) {
