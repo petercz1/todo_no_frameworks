da9275f4f94d8c95355bb462b9b230c88d9b4dc7
petercz1 <email.peter.carroll@gmail.com>
2019-05-24 10:47:48 +0800

    You'll never see all the places, or read all the books, but fortunately, they're not all recommended.

--- backend/receiveTask.php
+++ backend/receiveTask.php
@@ -12,7 +12,6 @@ class ReceiveTask
     public function init()
     {
         $data = json_decode(file_get_contents('php://input'), true);
-        \error_log(print_r($data, true));
         if(is_array($data)){
             foreach ($data as $task){
                 $this->router($task);
