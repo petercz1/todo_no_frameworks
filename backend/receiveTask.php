dd3350e186f5c4ec52f8151f658572d165bbcdf3
petercz1 <email.peter.carroll@gmail.com>
2019-05-24 10:50:47 +0800

    You are as I am with You.

--- backend/receiveTask.php
+++ backend/receiveTask.php
@@ -15,7 +15,7 @@ class ReceiveTask
         error_log(print_r($task, true));
         if(is_array($data)){
             foreach ($data as $task){
-                
+                error_log('');
                 $this->router($task);
             }
         }else{
