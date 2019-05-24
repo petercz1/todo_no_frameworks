f9f04e71ce64112b2f7c9d9ac93ab247ccb214db
petercz1 <email.peter.carroll@gmail.com>
2019-05-24 15:50:41 +0800

    You have been selected for a secret mission.

--- js/components/app-tasks.js
+++ js/components/app-tasks.js
@@ -14,7 +14,7 @@ class appTasks extends RootElement {
   // renders all people returned from 'getTasks()'
   renderData(tasks) {
     console.log('rendering...');
-    this.tasks
+    this.tasks;
     this.innerHTML = '';
     tasks.forEach(task => {
       this.append(new AppTask(task));
