393eda16e725a3844002454b9ac5d43ddbbf0956
petercz1 <email.peter.carroll@gmail.com>
2019-05-24 10:55:17 +0800

    Q:	How many Marxists does it take to screw in a light bulb? A:	None:  The light bulb contains the seeds of its own revolution.

--- backend/receiveTask.php
+++ backend/receiveTask.php
@@ -13,7 +13,7 @@ class ReceiveTask
     {
         $data = json_decode(file_get_contents('php://input'), true);
         error_log(print_r($data, true));
-        if(is_array($data)){
+        if($data['st']){
             foreach ($data as $task){
                 error_log('data is array of tasks');
                 $this->router($task);
