8d066c74f451e31bdbd7f04f6d81190f2af79ee9
petercz1 <email.peter.carroll@gmail.com>
2019-05-24 11:00:32 +0800

    Never be led astray onto the path of virtue.

--- backend/receiveTask.php
+++ backend/receiveTask.php
@@ -13,11 +13,11 @@ class ReceiveTask
     {
         $data = json_decode(file_get_contents('php://input'), true);
         error_log(print_r($data, true));
-        if(array_key_exists($data['status'])){
-            
-        }else{
-            
-            foreach ($data as $task){
+        if (array_key_exists($data['status'])) {
+            error_log('data is single task');
+            $this->router($data);
+        } else {
+            foreach ($data as $task) {
                 error_log('data is array of tasks');
                 $this->router($task);
             }
