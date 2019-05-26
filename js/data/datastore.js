32cdecd44abf5a8ed3a73f7ada80e0b27ad02a63
petercz1 <email.peter.carroll@gmail.com>
2019-05-26 19:00:29 +0800

    You could live a better life, if you had a better mind and a better body.

--- js/data/datastore.js
+++ js/data/datastore.js
@@ -72,6 +72,7 @@ class DataStore {
     // filter returns a copy of the array, which then replaces the original
     console.log('getting changed task');
     console.log(this.tasks);
+    //c heat to pass object by value
     let changedTask = JSON.parse(JSON.stringify(this.tasks.filter(task => task.changeTask == true)));
     this.tasks.find(task => task.changeTask == true).changeTask = false;
     console.log('after changed task');
