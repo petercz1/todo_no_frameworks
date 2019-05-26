b66b09774b8fe8621a6a60adf61a2ac31f94fe71
petercz1 <email.peter.carroll@gmail.com>
2019-05-26 14:07:52 +0800

    You have taken yourself too seriously.

--- js/components/app-task.js
+++ js/components/app-task.js
@@ -31,7 +31,7 @@ class appTask extends RootElement {
       setTimeout(() => {
         this.querySelector('div').classList.replace('closed', 'open');
       }, 100);
-      // tells
+      // tells pubsub to clear rolldown effect
       this.pubsub.publish('DisplayedTask', this.task);
     }
   }
