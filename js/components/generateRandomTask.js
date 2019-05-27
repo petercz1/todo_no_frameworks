a02ff8ae7c52ad9ccefa2082a4856fbd254d2e79
petercz1 <email.peter.carroll@gmail.com>
2019-05-27 12:06:29 +0800

    Try to have as good a life as you can under the circumstances.

--- js/components/generateRandomTask.js
+++ js/components/generateRandomTask.js
@@ -6,6 +6,19 @@ export default class GenerateRandomTask {
 
   generate(){
 	let item = this.tasks[Math.floor(Math.random() * this.tasks.length)];
+ // remove item from random tasklist
+ this.tasks = this.tasks.filter(task => {
+	return task != item;
+  })
 
+  // act like a typewriter, because I was bored
+  let counter = 0;
+  let typewriter = setInterval(function () {
+	document.getElementById("taskname").value += item[counter];
+	counter++;
+	if (counter > item.length - 1) {
+	  clearInterval(typewriter)
+	}
+  }, 50)
   }
 }
