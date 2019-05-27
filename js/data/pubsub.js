3fadb28be64f1e9cccfa7054f8002b1fd646755d
petercz1 <email.peter.carroll@gmail.com>
2019-05-27 13:55:42 +0800

    If you tell the truth you don't have to remember anything. 		-- Mark Twain

--- js/data/pubsub.js
+++ js/data/pubsub.js
@@ -7,7 +7,7 @@ class PubSub {
   }
 
   // this adds to an array everyone interested in some info being published
-  // here's a 
+  // here's a subscription from a
   // eg app-tasks is interested in any new tasks being published (newInfo - 'NewTask')
   // if there is a new task, request 'getTasks()' in the datastore (request - 'getTasks')
   // it could send a parameter if needed eg getTasks(6) (parameters - 6)
