285f54887f0effe7a05817f81ce0b2e8bf3a4ef9
petercz1 <email.peter.carroll@gmail.com>
2019-05-27 13:50:56 +0800

    It is so very hard to be an on-your-own-take-care-of-yourself-because-there-is-no-one-else-to-do-it-for-you grown-up.

--- js/data/pubsub.js
+++ js/data/pubsub.js
@@ -7,7 +7,7 @@ class PubSub {
   }
 
   // this adds to an array everyone interested in some info being published
-  // eg app-tasks is interested in any new asks 
+  // eg app-tasks is interested in any new tasks being published
   subscribe(newInfo, request, parameters, callback) {
     this.subscribers.push({
       newInfo,
