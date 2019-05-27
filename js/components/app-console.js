425d22e98cb06f639cfeaf5c866a86670324025b
petercz1 <email.peter.carroll@gmail.com>
2019-05-27 10:40:27 +0800

    Your step will soil many countries.

--- js/components/app-console.js
+++ js/components/app-console.js
@@ -18,7 +18,7 @@ class appConsole extends RootElement {
 		<div class="console">
 			<h3>Console monitoring stuff</h3>
 			<p>You have <span class="bold">${data.taskLength}</span> tasks, <span class="bold">${data.tasksChecked}</span> are completed</p>
-			<p>(Server data messages: )
+			<p>(Server data messages: ${})
 		</div>
 		`;
 	}
