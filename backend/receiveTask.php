834ed6cf6da28889444b44dc68604d459fb379a8
petercz1 <email.peter.carroll@gmail.com>
2019-05-27 12:38:18 +0800

    You own a dog, but you can only feed a cat.

--- backend/receiveTask.php
+++ backend/receiveTask.php
@@ -67,7 +67,7 @@ class ReceiveTask
         $tasks = json_decode(\file_get_contents('tasks.json'), true);
         array_unshift($tasks, $task);
         file_put_contents('tasks.json', json_encode($tasks));
-        return "{'server':'server added {$task["taskname"]}'}";
+        return "{'server':'server added {json_encode($task["taskname"]}'}";
     }
 
     public function changeTask(array $task): string
