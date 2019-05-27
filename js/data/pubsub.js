42daea18e656770f920551db19ddc75f8d349a4e
petercz1 <email.peter.carroll@gmail.com>
2019-05-27 13:43:47 +0800

    Writing is easy; all you do is sit staring at the blank sheet of paper until drops of blood form on your forehead. 		-- Gene Fowler

--- js/data/pubsub.js
+++ js/data/pubsub.js
@@ -20,7 +20,7 @@ class PubSub {
   }
 
   // when anyone publishes something it arrives here
-  // a string of newInfo eg 'NewTask' and an object of data
+  // a string of newInfo eg 'NewTask' and an object of data to publish
   publish(newInfo, data) {
 
     // publish the new/changed data
