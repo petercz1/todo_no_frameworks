7e15c7d30cc716a9a8cf5dc22cf9e572c06ee79f
petercz1 <email.peter.carroll@gmail.com>
2019-05-27 13:52:33 +0800

    Don't relax!  It's only your tension that's holding you together.

--- js/data/pubsub.js
+++ js/data/pubsub.js
@@ -8,7 +8,7 @@ class PubSub {
 
   // this adds to an array everyone interested in some info being published
   // eg app-tasks is interested in any new tasks being published (newInfo - 'NewTask')
-  // if there is a new task, fire 'getTasks()' in the datastore 
+  // if there is a new task, request 'getTasks()' in the datastore 
   subscribe(newInfo, request, parameters, callback) {
     this.subscribers.push({
       newInfo,
