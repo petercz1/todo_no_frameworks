6a956ae9cfd5920581bbdef578c03eba90786bf4
petercz1 <email.peter.carroll@gmail.com>
2019-05-27 12:25:31 +0800

    Perfect day for scrubbing the floor and other exciting things.

--- js/helpers/generateRandomTask.js
+++ js/helpers/generateRandomTask.js
@@ -13,7 +13,7 @@ export default new class GenerateRandomTask {
       return task != item;
     })
     // clear previous task from input box
-    document.getElementById("taskname").value;
+    document.getElementById("taskname").value = '';
 
     // act like a typewriter, because I was bored
     let counter = 0;
