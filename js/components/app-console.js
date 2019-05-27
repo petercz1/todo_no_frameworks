3a4f98c3614a616eb8be3e149e892c1def46d34b
petercz1 <email.peter.carroll@gmail.com>
2019-05-27 10:40:17 +0800

    You will be traveling and coming into a fortune.

--- js/components/app-console.js
+++ js/components/app-console.js
@@ -18,7 +18,7 @@ class appConsole extends RootElement {
 		<div class="console">
 			<h3>Console monitoring stuff</h3>
 			<p>You have <span class="bold">${data.taskLength}</span> tasks, <span class="bold">${data.tasksChecked}</span> are completed</p>
-			<p>(SErver data)
+			<p>(SErver data messages: )
 		</div>
 		`;
 	}
