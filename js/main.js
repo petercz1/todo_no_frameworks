e379e3169fa6ef31ba8bf388a795ade0f3d2dc54
petercz1 <email.peter.carroll@gmail.com>
2019-05-27 13:36:41 +0800

    For a light heart lives long. 		-- Shakespeare, "Love's Labour's Lost"

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ import './components/app-tasks.js';
 import './components/app-task.js';
 import './components/app-addtask.js';
 import './components/app-console.js';
-import './components/app-console.js';
+import './components/app-servermessages.js';
 import './data/sendTask.js';
 import fetchTasks from './data/fetchTasks.js';
 
