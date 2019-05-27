30fc5457ced89698c1bee3b8df1a0a22cb1d052c
petercz1 <email.peter.carroll@gmail.com>
2019-05-27 12:03:58 +0800

    Beauty and harmony are as necessary to you as the very breath of life.

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -7,7 +7,7 @@ class appAddTask extends RootElement {
     super();
     this.taskGen = GenerateRandomTask;
     // list of tasks for random task generator below -  speeds up testing
-    this.tasks = ['watch GOT', 'check FB', 'wash dishes', 'refine plans for Death Star', 'invent killer app', 'practice sarcasm', 'make fun of hipsters', 'drink more coffee', 'google "pimp my noodles"', 'complain more often', 'spike watercooler', 'write "idiot filter" for gmail', 'ignore todo list', 'beat personal best on snake'];
+    // this.tasks = ['watch GOT', 'check FB', 'wash dishes', 'refine plans for Death Star', 'invent killer app', 'practice sarcasm', 'make fun of hipsters', 'drink more coffee', 'google "pimp my noodles"', 'complain more often', 'spike watercooler', 'write "idiot filter" for gmail', 'ignore todo list', 'beat personal best on snake'];
 
     this.pubsub = PubSub;
     this.renderData();
