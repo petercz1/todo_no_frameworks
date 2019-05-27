fc533a6a2d4a4a6ad51330990c482dacbc6cbc93
petercz1 <email.peter.carroll@gmail.com>
2019-05-27 18:09:41 +0800

    Q:	What do you call a blind pre-historic animal? A:	Diyathinkhesaurus.
    Q:	What do you call a blind pre-historic animal with a dog?
    A:	Diyathinkhesaurus Rex.

--- js/data/sendTask.js
+++ js/data/sendTask.js
@@ -28,7 +28,7 @@ export default new class SendTask {
     // deal with the response
     if (!response.ok) {
       console.log(`${response.status}: ${response.statusText}`);
-      throw new Error('HTTP error, status = ' + response.status);
+      this.pubsub.publish('HTTP error, status = ' + response.status);
     }
     let json = await response.json();
     if (json.source == "server error") {
