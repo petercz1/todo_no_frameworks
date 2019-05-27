53ef9903ae98e7c2a5afdb9f6321b13bde76ea40
petercz1 <email.peter.carroll@gmail.com>
2019-05-27 12:03:43 +0800

    You have been selected for a secret mission.

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -5,7 +5,7 @@ import PubSub from '../data/pubsub.js';
 class appAddTask extends RootElement {
   constructor() {
     super();
-    this.tas
+    this.taskGen = 
     // list of tasks for random task generator below -  speeds up testing
     this.tasks = ['watch GOT', 'check FB', 'wash dishes', 'refine plans for Death Star', 'invent killer app', 'practice sarcasm', 'make fun of hipsters', 'drink more coffee', 'google "pimp my noodles"', 'complain more often', 'spike watercooler', 'write "idiot filter" for gmail', 'ignore todo list', 'beat personal best on snake'];
 
