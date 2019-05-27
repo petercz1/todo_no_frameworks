7482968b2274674e67f0fb1d27b61ef61fbfd5dd
petercz1 <email.peter.carroll@gmail.com>
2019-05-27 10:48:13 +0800

    The smallest worm will turn being trodden on. 		-- William Shakespeare, "Henry VI"

--- js/data/datastore.js
+++ js/data/datastore.js
@@ -97,7 +97,7 @@ class DataStore {
   // handles returning ServerData
   setServerData(data){
     console.log(data);
-    // piping returning server data 
+    // piping returning server data here
     this.meta.serverdata = data;
   }
   // clears rolldown css effect after task is added to list and displayed
