5fa9e4bb6e5d64357ac9ddb5ca569e98fb5b92bb
petercz1 <email.peter.carroll@gmail.com>
2019-05-27 12:05:28 +0800

    You have taken yourself too seriously.

--- js/components/generateRandomTask.js
+++ js/components/generateRandomTask.js
@@ -3,5 +3,6 @@ export default class GenerateRandomTask {
     this.tasks = ['watch GOT', 'check FB', 'wash dishes', 'refine plans for Death Star', 'invent killer app', 'practice sarcasm', 'make fun of hipsters', 'drink more coffee', 'google "pimp my noodles"', 'complain more often', 'spike watercooler', 'write "idiot filter" for gmail', 'ignore todo list', 'beat personal best on snake'];
 
   }
+
   
 }
