4e4ce4f32e5fdf38c849cd6d1f9e3f00f6599441
petercz1 <email.peter.carroll@gmail.com>
2019-05-27 12:11:27 +0800

    Give thought to your reputation.  Consider changing name and moving to a new town.

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -36,29 +36,6 @@ class appAddTask extends RootElement {
     // generate new random task
     this.taskGen.generate();
   }
-
-  // random task generator for testing, saves having to make up a task and type it into the input box
-  // generateNewTask() {
-  //   this.querySelector('#taskname').value = '';
-
-  //   // randomly select a task
-  //   let item = this.tasks[Math.floor(Math.random() * this.tasks.length)];
-
-  //   // remove item from random tasklist
-  //   this.tasks = this.tasks.filter(task => {
-  //     return task != item;
-  //   })
-
-  //   // act like a typewriter, because I was bored
-  //   let counter = 0;
-  //   let typewriter = setInterval(function () {
-  //     document.getElementById("taskname").value += item[counter];
-  //     counter++;
-  //     if (counter > item.length - 1) {
-  //       clearInterval(typewriter)
-  //     }
-  //   }, 50)
-  // }
 }
 
 customElements.define('app-addtask', appAddTask);
