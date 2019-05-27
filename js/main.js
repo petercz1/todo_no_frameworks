fa1a009ba31df12ede8f214e39084c1396e259cf
petercz1 <email.peter.carroll@gmail.com>
2019-05-27 13:36:37 +0800

    Q:	What does a WASP Mom make for dinner? A:	A crisp salad, a hearty soup, a lovely entree, followed by 	a delicious dessert.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ import './components/app-tasks.js';
 import './components/app-task.js';
 import './components/app-addtask.js';
 import './components/app-console.js';
+import './components/app-console.js';
 import './data/sendTask.js';
 import fetchTasks from './data/fetchTasks.js';
 
