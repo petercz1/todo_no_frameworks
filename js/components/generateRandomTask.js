3ce38ece73efdfc31f51d54bfed41b223e59783a
petercz1 <email.peter.carroll@gmail.com>
2019-05-27 12:09:24 +0800

    What I tell you three times is true. 		-- Lewis Carroll

--- js/components/generateRandomTask.js
+++ js/components/generateRandomTask.js
@@ -2,7 +2,6 @@ export default class GenerateRandomTask {
   constructor() {
     // list of tasks for random task generator below -  speeds up testing
     this.tasks = ['watch GOT', 'check FB', 'wash dishes', 'refine plans for Death Star', 'invent killer app', 'practice sarcasm', 'make fun of hipsters', 'drink more coffee', 'google "pimp my noodles"', 'complain more often', 'spike watercooler', 'write "idiot filter" for gmail', 'ignore todo list', 'beat personal best on snake'];
-
   }
 
   generate() {
