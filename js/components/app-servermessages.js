318ee0c3148feb0566ca7a06f32f31e43538fff3
petercz1 <email.peter.carroll@gmail.com>
2019-05-27 17:08:21 +0800

    Beware of a tall black man with one blond shoe.

--- js/components/app-servermessages.js
+++ js/components/app-servermessages.js
@@ -3,25 +3,26 @@ import PubSub from '../data/pubsub.js';
 
 export default class appServerMessages extends RootElement {
   constructor() {
-		super();
-		this.pubsub = PubSub;
-		this.renderData(this.pubsub.getData('getMeta', null));
-		this.pubsub.subscribe('ServerMessage', 'getMeta', null, this.renderData);
+    super();
+    this.pubsub = PubSub;
+    this.renderData(this.pubsub.getData('getMeta', null));
+    this.pubsub.subscribe('ServerMessage', 'getMeta', null, this.renderData);
   }
 
-	renderData(data){
-		this.innerHTML = `
+  renderData(data) {
+    this.innerHTML = `
 		<div class="server closed">
 			<h3>Server</h3>
 			<p>${data.message.server}</p>
 		</div>
 		`;
-	}
-// setTimeout needed because connectedCallback doesn't wait for child elements to render
-connectedCallback() {
-	setTimeout(() => {
-        this.querySelector('div').classList.replace('closed', 'open');
-      }, 100);
+  }
+  // setTimeout needed because connectedCallback doesn't wait for child elements to render
+  connectedCallback() {
+    setTimeout(() => {
+      this.querySelector('div').classList.replace('closed', 'open');
+    }, 100);
+  }
 }
 
 customElements.define('app-servermessages', appServerMessages);
