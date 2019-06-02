de9293c50a803aac808bcd86825cf0c68c08c2e9
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 18:08:01 +0800

    The lunatic, the lover, and the poet, Are of imagination all compact... 		-- Wm. Shakespeare, "A Midsummer Night's Dream"

--- js/data/datastore.js
+++ js/data/datastore.js
@@ -37,7 +37,7 @@ class DataStore {
   }
   getNewTask() {
     // return this.clientTasks.reduce((prev, current) => (prev.id > current.id) ? prev : current);
-    return this.clientTasks.filter(clientTask =>)
+    return this.clientTasks.filter(clientTask => clientTask)
   }
   getChangeTask() {
     // use the JSON.parse/stringify hack to make a copy of task array
