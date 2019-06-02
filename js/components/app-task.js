e9223991b200d87f8b4d837388d3943e98ce658c
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 13:42:44 +0800

    Your aim is high and to the right.

--- js/components/app-task.js
+++ js/components/app-task.js
@@ -1,6 +1,7 @@
 import RootElement from '../helpers/app-rootelement.js';
 import PubSub from '../data/pubsub.js';
 
+// ES6 webcomponent <app-servermessages>
 export default class appTask extends RootElement {
   constructor(task) {
     super();
