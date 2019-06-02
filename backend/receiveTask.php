7b3a21c8a1d9e04504ffe1410eb4ab4aab7043b4
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 18:50:40 +0800

    O, it is excellent To have a giant's strength; but it is tyrannous To use it like a giant. 		-- Shakespeare, "Measure for Measure", II, 2

--- backend/receiveTask.php
+++ backend/receiveTask.php
@@ -8,7 +8,7 @@ namespace chipbug\todo;
 ini_set("log_errors", "1");
 ini_set("error_log", getcwd() . "/debug.log");
 
-require './newTask.php';
+require './addTask.php';
 require './changeTask.php';
 require './deleteTask.php';
 
