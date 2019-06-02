a78453dd4af43e6021e8e0cc606871bd5320ce56
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 14:11:42 +0800

    Accent on helpful side of your nature.  Drain the moat.

--- js/components/app-tasks.js
+++ js/components/app-tasks.js
@@ -3,6 +3,7 @@ import AppTask from './app-task.js';
 import PubSub from '../data/pubsub.js';
 
 // ES6 webcomponent <app-tasks>
+// container for 
 export default class appTasks extends RootElement {
   constructor() {
     super();
