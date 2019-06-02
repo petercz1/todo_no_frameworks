74b5fbc094418c2700b6926a565bd9886653568d
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 18:47:45 +0800

    You'll wish that you had done some of the hard things when they were easier to do.

--- backend/evilBoss.php
+++ backend/evilBoss.php
@@ -57,7 +57,8 @@ class EvilBoss
         // send update every 10 - 15 seconds
         $timeint = 1000 * rand(10, 15);
 
-        $evilTask = json_decode($evilTask)
+        $evilTask = json_decode($evilTask);
+        
         try {
             header("Cache-Control: no-cache");
             header("Content-Type: text/event-stream");
