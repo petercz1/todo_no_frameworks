10df5e7f546a6cce76e2b41a72d0a57fce27b718
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 13:43:38 +0800

    Day of inquiry.  You will be subpoenaed.

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -16,6 +16,7 @@ export default class appAddTask extends RootElement {
     <input type="text" id="taskname" placeholder="your new task"></input>
     <button id="newTask">add task</button>
     `;
+    // add listener
     document.querySelector('#newTask').addEventListener('click', this.addtask);
    
     this.generateRandomTask.generate(); // generate new random task
