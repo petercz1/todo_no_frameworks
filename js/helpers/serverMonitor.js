f2e56a05d83e3e3751133c26483a7091804d2e49
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 15:14:07 +0800

    You will be honored for contributing your time and skill to a worthy cause.

---
+++ js/helpers/serverMonitor.js
@@ -0,0 +1,23 @@
+import PubSub from '../data/pubsub.js';
+
+// monitors when evilBoss adds extra t
+export default class ServerMonitor {
+
+  constructor() {
+    this.pubsub = PubSub;
+    this.eventSource = new EventSource('../../backend/evilBoss.php');
+    this.monitor = this.monitor.bind(this);
+    this.evilBossAddedTask = this.evilBossAddedTask.bind(this);
+  }
+  
+  monitor() {
+    this.eventSource.addEventListener("evilBossAddedTask", this.evilBossAddedTask);
+  }
+
+  evilBossAddedTask(event) {
+    let data = JSON.parse(event.data);
+    if (event.data != "{}") {
+      this.pubsub.publish('ServerTask', data);
+    }
+  }
+}
