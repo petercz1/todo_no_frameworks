66581f63973c9cfbfcc1d8ce52aeda2850d6ea11
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 14:51:06 +0800

    Your domestic life may be harmonious.

--- js/data/pubsub.js
+++ js/data/pubsub.js
@@ -45,7 +45,6 @@ export default new class PubSub {
 
   getData(request) {
     // this handles non-publish/subscribe requests for info such as getting totals 
-    // before publishing a new record eg 'getCheckedTotal()'
     return this.datastore.getRequest({
       request
     });
