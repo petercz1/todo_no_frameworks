075632959121fb27193221dd5849b0a6857a9dc8
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 18:07:56 +0800

    Q:	How do you stop an elephant from charging? A:	Take away his credit cards.

--- js/data/datastore.js
+++ js/data/datastore.js
@@ -37,7 +37,7 @@ class DataStore {
   }
   getNewTask() {
     // return this.clientTasks.reduce((prev, current) => (prev.id > current.id) ? prev : current);
-    return this.clientTasks.filter(client)
+    return this.clientTasks.filter(clientTask =>)
   }
   getChangeTask() {
     // use the JSON.parse/stringify hack to make a copy of task array
