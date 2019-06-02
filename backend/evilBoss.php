a0f0a627dfd24f3ed1fbfe2d35243c3a25224b81
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 18:52:03 +0800

    You will be called upon to help a friend in trouble.

--- backend/evilBoss.php
+++ backend/evilBoss.php
@@ -41,7 +41,7 @@ class EvilBoss
         //array_unshift($serverTasks, $evilTask);
         //file_put_contents('tasks.json', json_encode($serverTasks));
         // send evilTask to client
-        $newTask = new NewTask();
+        $addTask = new NewTask();
         $this->sse($newTask->init($evilTask));
     }
     
