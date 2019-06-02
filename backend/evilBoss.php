6101b1f39417dae5b2a3c7420f07ddf29b4f9f48
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 12:53:48 +0800

    One of the most striking differences between a cat and a lie is that a cat has only nine lives. 		-- Mark Twain, "Pudd'nhead Wilson's Calendar"

--- backend/evilBoss.php
+++ backend/evilBoss.php
@@ -3,8 +3,8 @@ declare(strict_types=1);
 namespace chipbug\todo;
 
 // dumps debug stuff into a debug.log file in this directory
-// error_log('stuff'); for simple debug.
-// error_log(print_r($my_array, true)); for arrays
+// error_log('stuff'); for simple debug,
+// error_log(print_r($my_array, true)); for arrays.
 ini_set("log_errors", "1");
 ini_set("error_log", getcwd() . "/debug.log");
 
