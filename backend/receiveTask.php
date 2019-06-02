94a952f2a8011af7ee2d73efe686642722a46d4f
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 18:43:07 +0800

    You have a strong appeal for members of your own sex.

--- backend/receiveTask.php
+++ backend/receiveTask.php
@@ -35,7 +35,7 @@ class ReceiveTask
         sleep(2); // mimic a 'slow' server
         if ($this->clientTask['deleteTask']) {
             $newTask = new NewTask();
-            echo $newTask->init($this->clientTask)
+            echo $newTask->init($this->clientTask);
             echo $this->deleteTask($this->clientTask);
         } elseif ($this->clientTask['changeTask']) {
             echo $this->changeTask($this->clientTask);
