efdf20a914a37d165f678d8df914102724d0f82a
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 13:43:46 +0800

    Your manuscript is both good and original, but the part that is good is not original and the part that is original is not good. 		-- Samuel Johnson

--- js/components/app-addtask.js
+++ js/components/app-addtask.js
@@ -16,7 +16,7 @@ export default class appAddTask extends RootElement {
     <input type="text" id="taskname" placeholder="your new task"></input>
     <button id="newTask">add task</button>
     `;
-    // add listener
+    // add listener for add
     document.querySelector('#newTask').addEventListener('click', this.addtask);
    
     this.generateRandomTask.generate(); // generate new random task
