3784a676bd1e6ed24520dc5aec286804b9a3f148
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 14:06:25 +0800

    There is no distinctly native American criminal class except Congress. 		-- Mark Twain

--- js/components/app-console.js
+++ js/components/app-console.js
@@ -8,8 +8,8 @@ export default class appConsole extends RootElement {
 		this.pubsub = PubSub;
 		// initial render with metadata
 		this.renderData(this.pubsub.getData('getMeta', null));
-		
-		// subscribe to any of these events and get metadata
+
+		// subscribe to any of these events,  get metadata
 		this.pubsub.subscribe('NewTask', 'getMeta', this.renderData);
 		this.pubsub.subscribe('ChangeTask', 'getMeta', this.renderData);
 		this.pubsub.subscribe('DeleteTask', 'getMeta', this.renderData);
