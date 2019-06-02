efec34ca852cfd7469b15db5a315db9d70e2f324
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 12:47:33 +0800

    F.S. Fitzgerald to Hemingway: 	"Ernest, the rich are different from us." Hemingway: 	"Yes.  They have more money."

--- backend/evilBoss.php
+++ backend/evilBoss.php
@@ -32,7 +32,7 @@ class EvilBoss
             "css" => "closed",
         );
         // set id
-        // if no tas
+        // if no tasklist is empty
         empty($serverTasks) ? $evilTask['id'] = 1: $evilTask['id'] = max(array_column($serverTasks, 'id')) + 1;
         
         // add to the start of task array
