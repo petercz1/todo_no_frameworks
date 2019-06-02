ba4e3deaeafebc3d49303bcc158736413896158a
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 13:13:44 +0800

    You could live a better life, if you had a better mind and a better body.

--- backend/receiveTask.php
+++ backend/receiveTask.php
@@ -70,7 +70,6 @@ class ReceiveTask
         //get tasks from file
         $serverTasks = json_decode(\file_get_contents('tasks.json'), true);
         foreach ($serverTasks as &$serverTask) {
-            //$serverTask = (array)$serverTask;
             if ($serverTask['id'] == $clientTask['id']) {
                 if ($clientTask['checked']) {
                     $clientTask['message'] = "server checked task: " . $clientTask['taskname'];
