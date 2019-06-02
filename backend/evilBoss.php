e2f85ba8ca469d531c42374fc6168d5b12704bc7
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 12:53:12 +0800

    Beware of a dark-haired man with a loud tie.

--- backend/evilBoss.php
+++ backend/evilBoss.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 namespace chipbug\todo;
 
 // dumps debug stuff into a debug.log file in this directory
-// error_log(')
+// error_log('stuff')
 ini_set("log_errors", "1");
 ini_set("error_log", getcwd() . "/debug.log");
 
