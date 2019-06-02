9d6b449f61b73fc6f60ff0052e806a6a3e50d936
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 15:12:23 +0800

    He draweth out the thread of his verbosity finer than the staple of his argument. 		-- William Shakespeare, "Love's Labour's Lost"

--- js/data/sendTask.js
+++ js/data/sendTask.js
@@ -7,7 +7,7 @@ export default new class SendTask {
   constructor() {
     this.pubsub = PubSub;
     this.sendData = this.sendData.bind(this);
-    // 
+    // listen for any of these
     this.pubsub.subscribe('NewTask', 'getNewTask', this.sendData);
     this.pubsub.subscribe('ChangeTask', 'getChangeTask', this.sendData);
     this.pubsub.subscribe('DeleteTask', 'getDeleteTask', this.sendData);
