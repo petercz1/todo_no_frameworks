40683a6e46941dad56788601a70a0cd889b343da
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 12:53:33 +0800

    You will soon meet a person who will play an important role in your life.

--- backend/evilBoss.php
+++ backend/evilBoss.php
@@ -4,7 +4,7 @@ namespace chipbug\todo;
 
 // dumps debug stuff into a debug.log file in this directory
 // error_log('stuff'); for simple debug.
-// error_log(print_r)
+// error_log(print_r($my_array))
 ini_set("log_errors", "1");
 ini_set("error_log", getcwd() . "/debug.log");
 
