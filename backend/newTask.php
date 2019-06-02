7477302f6bb941ef4473043edcce677e29138adf
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 18:16:14 +0800

    Q:	What do you call a half-dozen Indians with Asian flu? A:	Six sick Sikhs (sic).

---
+++ backend/newTask.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+namespace chipbug\todo;
+
+// dumps debug stuff into a debug.log file in this directory
+// error_log('stuff'); for simple debug,
+// error_log(print_r($my_array, true)); for arrays.
+ini_set("log_errors", "1");
+ini_set("error_log", getcwd() . "/debug.log");
+
+class ChangeTask{
+
+	public function init()
+	{
+		# code...
+	}
+}
