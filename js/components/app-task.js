dd1d77cb3767c03dc44b17ad7fed45c397f57791
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 14:08:23 +0800

    Always do right.  This will gratify some people and astonish the rest. 		-- Mark Twain

--- js/components/app-task.js
+++ js/components/app-task.js
@@ -19,7 +19,7 @@ export default class appTask extends RootElement {
       <span>&#128465;</span>
     </div>
     `;
-    // set html checkbox to 
+    // set html checkbox to task
     this.querySelector('input').checked = this.task.checked;
     if (this.task.checked) {
       this.querySelector('label').classList.add('checked');
