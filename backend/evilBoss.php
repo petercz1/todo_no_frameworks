df4410bdd4d501fee0b9ae6cc750d021fb147f23
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 14:37:05 +0800

    Your lucky number is 3552664958674928.  Watch for it everywhere.

--- backend/evilBoss.php
+++ backend/evilBoss.php
@@ -42,7 +42,7 @@ class EvilBoss
         array_unshift($serverTasks, $evilTask);
         file_put_contents('tasks.json', json_encode($serverTasks));
         // send fresh copy of all serverTasks
-        $this->sse($serverTasks);
+        $this->sse($evilTask);
     }
     
     /**
