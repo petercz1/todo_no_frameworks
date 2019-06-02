edd0e27d7555d73101160a9ce51189f3e1878578
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 15:09:33 +0800

    You will gain money by an illegal action.

--- js/data/fetchTasks.js
+++ js/data/fetchTasks.js
@@ -1,7 +1,7 @@
 // fetches videos from server, catches any network/server errors, returns json list of video details
 import PubSub from './pubsub.js';
 
-// async retrieval of tasks from task.json on server
+// async retrieval of tasks from tasks.json on server
 export default class FetchTasks {
 
   constructor() {
