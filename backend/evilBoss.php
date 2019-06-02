14b5fbdf16f59ae75f1bd80ceed00790a4ded3e4
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 18:51:46 +0800

    Q:	What's the difference between Bell Labs and the Boy Scouts of America? A:	The Boy Scouts have adult supervision.

--- backend/evilBoss.php
+++ backend/evilBoss.php
@@ -8,7 +8,7 @@ namespace chipbug\todo;
 ini_set("log_errors", "1");
 ini_set("error_log", getcwd() . "/debug.log");
 
-require_once 'newTask.php';
+require_once 'addTask.php';
 
 (new EvilBoss)->init();
 
