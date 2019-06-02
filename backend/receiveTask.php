0dcc2db6eecda6c28fc8d5079f1b9109a21cbeaa
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 18:43:19 +0800

    You get along very well with everyone except animals and people.

--- backend/receiveTask.php
+++ backend/receiveTask.php
@@ -34,7 +34,7 @@ class ReceiveTask
 
         sleep(2); // mimic a 'slow' server
         if ($this->clientTask['deleteTask']) {
-            $deleteTask = new NewTask();
+            $deleteTask = new DeleteTask();
             echo $newTask->init($this->clientTask);
             echo $this->deleteTask($this->clientTask);
         } elseif ($this->clientTask['changeTask']) {
