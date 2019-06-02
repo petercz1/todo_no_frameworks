6bf77be8c99fc5018ccaef6b569c2a0436679e8f
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 13:42:22 +0800

    Q:	Why do firemen wear red suspenders? A:	To conform with departmental regulations concerning uniform dress.

---
+++ js/components/app-servermessages.js
@@ -0,0 +1,27 @@
+import RootElement from '../helpers/app-rootelement.js';
+import PubSub from '../data/pubsub.js';
+
+
+export default class appServerMessages extends RootElement {
+  constructor() {
+    super();
+    this.pubsub = PubSub;
+    this.renderData(this.pubsub.getData('getMeta'));
+    this.pubsub.subscribe('NewTask', 'getMeta', this.renderData);
+		this.pubsub.subscribe('ChangeTask', 'getMeta', this.renderData);
+		this.pubsub.subscribe('DeleteTask', 'getMeta', this.renderData);
+		this.pubsub.subscribe('ServerTask', 'getMeta', this.renderData);
+		this.pubsub.subscribe('ServerTasks', 'getMeta', this.renderData);
+  }
+
+  renderData(metaData) {
+    this.innerHTML = `
+		<div class="server">
+			<h3>Client/Server messages</h3>
+			<p class="serverMessage">${metaData.message}</p>
+		</div>
+		`;
+  }
+}
+
+customElements.define('app-servermessages', appServerMessages);
