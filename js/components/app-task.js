3f8a36cb083b80853b84ccaf79215ea6a7306c3b
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 14:07:57 +0800

    Just to have it is enough.

--- js/components/app-task.js
+++ js/components/app-task.js
@@ -19,6 +19,7 @@ export default class appTask extends RootElement {
       <span>&#128465;</span>
     </div>
     `;
+    
     this.querySelector('input').checked = this.task.checked;
     if (this.task.checked) {
       this.querySelector('label').classList.add('checked');
