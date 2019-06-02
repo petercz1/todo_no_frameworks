f3e31ba3e4ab2496cb84b24b8ca2533996f8be1d
petercz1 <email.peter.carroll@gmail.com>
2019-06-02 18:42:53 +0800

    You will be run over by a beer truck.

--- backend/receiveTask.php
+++ backend/receiveTask.php
@@ -35,7 +35,7 @@ class ReceiveTask
         sleep(2); // mimic a 'slow' server
         if ($this->clientTask['deleteTask']) {
             $newTask = new NewTask();
-            echo $new
+            echo $newTask->init()
             echo $this->deleteTask($this->clientTask);
         } elseif ($this->clientTask['changeTask']) {
             echo $this->changeTask($this->clientTask);
