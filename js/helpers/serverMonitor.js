84438c5f10880249aa530c5085b7275e38dec64b
petercz1 <email.peter.carroll@gmail.com>
2019-06-03 10:21:45 +0800

    Your life would be very empty if you had nothing to regret.

--- js/helpers/serverMonitor.js
+++ js/helpers/serverMonitor.js
@@ -18,6 +18,7 @@ export default class ServerMonitor {
   evilBossAddedTask(event) {
     let data = JSON.parse(event.data);
     if (event.data != "{}") {
+      console.log(data);
       this.pubsub.publish('ServerTask', data);
     }
   }
